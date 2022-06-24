c3eeecd19768272028b5e1db7c537e495cae8870
Rahul <70031623+CodingKite@users.noreply.github.com>
2022-06-24 07:50:55 +0530

    first commit dsa

---
+++ 1)gettingStarted/_1_printOutput.java
@@ -0,0 +1,14 @@
+package compiledCode;
+
+public class _1_printOutput{
+  public static void main(String[] args) {
+    System.out.println("*****");    
+    System.out.println("   *");    
+    System.out.println("  *");    
+    System.out.println(" *");    
+    System.out.println("*****");    
+
+  }
+  
+}
+

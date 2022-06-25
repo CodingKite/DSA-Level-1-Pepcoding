98d9e7a118490530a1f5e59662ba773444ed4779
Rahul <70031623+CodingKite@users.noreply.github.com>
2022-06-25 21:51:44 +0530

    wrote first java program

--- 1)gettingStarted/_1_printOutput.java
+++ 1)gettingStarted/_1_printOutput.java
@@ -1,11 +1,51 @@
+import java.util.Scanner;
+
 public class _1_printOutput{
   public static void main(String[] args) {
+
+    _1_printOutput obj = new _1_printOutput();
+    obj.printZ();
+    System.out.println();
+    obj.printPattern();
+    System.out.println();
+
+    obj.printZMethod2();
+
+  }
+
+  void printZ(){
     System.out.println("*****");    
     System.out.println("   *");    
     System.out.println("  *");    
     System.out.println(" *");    
     System.out.println("*****");    
+  }
 
+  void printZMethod2(){
+    Scanner scn = new Scanner(System.in);
+    System.out.print("Enter an number: ");
+    int num = scn.nextInt();
+    scn.close();
+
+    for(int i = 1; i <= num; i++){
+      if(i == 1 || i == num){
+        for(int k = 1;k <= num; k++ ){
+          System.out.print("*");
+        }
+        System.out.println();
+      }else{
+        for(int j = 1; j<(num-i+1);j++){
+          // System.out.print(num-i);
+          System.out.print(" ");
+        }
+        System.out.println("*");
+      }  
+    }
+
+  }
+
+  void printPattern(){
+    System.out.println("*\n**\n***\n****\n*****");
   }
   
 }
